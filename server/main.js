311ac8943bed6287e1e3f9cb3ac0952609aa0c85
Daniel Delgadillo - Software Engineer <danieldelgadilloh@gmail.com>
2017-07-12 19:56:02 -0500

    change db

--- server/main.js
+++ server/main.js
@@ -389,8 +389,8 @@ Meteor.publish('users', function (search) {
 SyncedCron.add({
   name: 'Intereses',
   schedule: function(parser) {
-    //return parser.text('every 1 hour');
-    return parser.text('every 1 seconds');
+    return parser.text('every 1 hour');
+    //return parser.text('every 1 seconds');
   },
   job: function() {
 
